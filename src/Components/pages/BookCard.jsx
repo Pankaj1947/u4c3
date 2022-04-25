897b4ab5d61c89f6e9a6aeaa045fd427cbf1aa49
Pankaj <pankajkr885@gmail.com>
2022-04-25 17:54:54 +0530

    done

---
+++ src/Components/pages/BookCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+export const Flex = styled.div`
+//  add required style
+border-radius:10px;
+margin:10px;
+box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+`;
+export const BookCard = (props) => {
+  const { id, title, isbn, pageCount, thumbnailUrl } = props.item;
+
+  return (
+    <>
+      <Link to={`/books/${id}`}>
+        <Flex >
+          <img src={thumbnailUrl} alt={title} height="200px" width={"200px"} />
+          <h3>{title}</h3>
+          <h4>{isbn}</h4>
+        </Flex>
+      </Link>
+    </>
+  );
+};
---
+++ src/Components/pages/Books.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+
+import { BookCard } from "./BookCard";
+import styled, { css } from "styled-components";
+
+export const Grid = styled.div`
+  display:grid;
+  width: 80%;
+  grid-template-columns:repeat(2,1fr);
+  grid-template-rows:repeat(auto,260px);
+  gap:50px;
+  justify-content:center;
+`;
+
+const Books = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // make a GET request to http://localhost:8080/books to get all the books data
+    const getData = async () => {
+      try {
+        let res = await fetch("http://localhost:8080/books")
+        let data = await res.json();
+        setData(data)
+
+      } catch (error) {
+
+      }
+    }
+    getData()
+  }, []);
+
+  return (
+    <>
+      <h1>Books</h1>
+      <Grid data-testid="books-container">
+        {
+          // map thorugh the data and use <BookCard/> component to display each book
+          data.map((item) => {
+            return (
+              <>
+                <BookCard item={item} key={item.id} />
+              </>
+            )
+          })
+        }
+      </Grid>
+    </>
+  );
+};
+export default Books;
